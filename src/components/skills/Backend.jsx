4b4e68901411c29f526e608f7156ebb0b01aa710
DAVID R <103181452+drondiodev@users.noreply.github.com>
2023-08-02 09:25:42 +0200

    amélioration sémantique composant skills

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,49 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const Backend = () => {
     return (
-        <div className="skills__content">
-            <i className="uil uil-server skills__icon"></i>
-        <h3 className="skills__title">Backend</h3>
-        <div className="skills__box">
-
-            <div className="skills__group">
-            <div className="skills__data">
-                <i className="devicon-nodejs-plain skills__icon"></i>
-
-                <div>
-                <h3 className="skills__name">NodeJs</h3>
-                </div>
+        <section className="skills__content" aria-label="Compétences backend">
+            <header>
+                <i className="uil uil-server skills__icon"></i>
+                <h2 className="skills__title">Backend</h2>
+            </header>
+            <div className="skills__box">
+                <ul className="skills__group" role="list">
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-nodejs-plain skills__icon"></i>
+                        <p className="skills__name">NodeJs</p>
+                    </li>
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-express-original skills__icon"></i>
+                        <p className="skills__name">ExpressJS</p>
+                    </li>
+                </ul>
+                <ul className="skills__group" role="list">
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-postgresql-plain skills__icon"></i>
+                        <p className="skills__name">PostgreSQL</p>
+                    </li>
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-sequelize-plain skills__icon"></i>
+                        <p className="skills__name">Sequelize</p>
+                    </li>
+                </ul>
             </div>
-
-            <div className="skills__data">
-                <i className="devicon-express-original skills__icon"></i>
-
-                <div>
-                <h3 className="skills__name">ExpressJS</h3>
-                </div>
-            </div>
-            </div>
-
-            <div className="skills__group">
-            <div className="skills__data">
-                <i className="devicon-postgresql-plain skills__icon"></i>
-
-                <div>
-                <h3 className="skills__name">PostgreSQL</h3>
-                </div>
-            </div>
-
-            <div className="skills__data">
-                <i className="devicon-sequelize-plain skills__icon"></i>
-
-                <div>
-                <h3 className="skills__name">Sequelize</h3>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
+        </section>
     )
 }
 
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -2,53 +2,43 @@ import React from "react";
 
 const Frontend = () => {
     return (
-        <div className="skills__content">
-            <i className="uil uil-arrow skills__icon"></i>
-        <h3 className="skills__title">Frontend</h3>
+        <section className="skills__content" aria-label="Compétences frontend">
+            <header>
+                <i className="uil uil-arrow skills__icon" alt=""></i> 
+                <h2 className="skills__title">Frontend</h2>
+            </header>
             <div className="skills__box">
 
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i className="devicon-html5-plain skills__icon"></i>
-                        <div>
-                        <h3 className="skills__name">HTML</h3>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="devicon-css3-plain skills__icon"></i>
-                        <div>
-                            <h3 className="skills__name">CSS</h3>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="devicon-javascript-plain skills__icon"></i>
-                        <div>
-                        <h3 className="skills__name">JavaScript</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i className="devicon-react-original skills__icon"></i>
-                        <div>
-                        <h3 className="skills__name">React</h3>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="devicon-jamstack-original skills__icon"></i>
-                        <div>
-                        <h3 className="skills__name">Jamstack</h3>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="devicon-git-plain skills__icon"></i>
-                        <div>
-                        <h3 className="skills__name">Git/GitHub</h3>
-                        </div>
-                    </div>
-                </div>
+                <ul className="skills__group" role="list">
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-html5-plain skills__icon" alt=""></i>
+                        <p className="skills__name">HTML</p>
+                    </li>
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-css3-plain skills__icon" alt=""></i>
+                        <p className="skills__name">CSS</p>
+                    </li>
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-javascript-plain skills__icon" alt=""></i>
+                        <p className="skills__name">JavaScript</p>
+                    </li>
+                </ul>
+                <ul className="skills__group" role="list">
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-react-original skills__icon" alt=""></i>
+                        <p className="skills__name">React</p>
+                    </li>
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-jamstack-original skills__icon" alt=""></i>
+                        <p className="skills__name">Jamstack</p>
+                    </li>
+                    <li className="skills__data" role="listitem">
+                        <i className="devicon-git-plain skills__icon" alt=""></i>
+                        <p className="skills__name">Git/GitHub</p>
+                    </li>
+                </ul>
             </div>
-        </div>
+        </section>
 
 
     );
