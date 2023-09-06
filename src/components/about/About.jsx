9ed6e11f5fc599fa51a1f1a9f929322583588815
DAVID R <103181452+drondiodev@users.noreply.github.com>
2023-09-06 14:02:43 +0200

    modification lien resume.pdf

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
                 <div>
                     <div className="skills__box">
                         <i className="uil uil-file-download-alt skills__icon" alt=""></i>
-                        <p className="skills__name active-link"><a href="https://drive.google.com/file/d/14f0yVKzF_QJNouBZKFuLd7E46_mh9ge3/view?usp=drive_link" download="Mon CV en pdf">Téléchargez mon CV ici</a></p>
+                        <p className="skills__name active-link"><a href="https://drive.google.com/file/d/1Fr7fwtAYYKdp3VbfSvuXTOWiVbCfspdG/view?usp=drive_link" download="Mon CV en pdf">Téléchargez mon CV ici</a></p>
                     </div>
                 </div>
             </div>
