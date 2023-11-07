b8d8b41b47a4bad485842fffdaf192019609f659
DAVID R <103181452+drondiodev@users.noreply.github.com>
2023-11-07 12:37:29 +0100

    correctifs

--- src/components/workingproject/working.jsx
+++ src/components/workingproject/working.jsx
@@ -4,16 +4,18 @@ import WorkItems from "../work/WorkItems";
 import { projectsData } from "../work/Data";
 
 const Working = () => {
-  const project = projectsData.find(p => p.id === 1);
-    return (
-        <section className="skills section">
-        <h2 className="section__title">Coming soon</h2>
-        <h3 className="section__subtitle">Sur quoi je travaille actuellement</h3>
-        <div className="container_working">
-         <WorkItems item={project} />
-        </div>
-        </section>
-    );
+  const project = projectsData.find((p) => p.id === 1);
+  return (
+    <section className="skills section">
+      <h2 className="section__title">
+        Coming <span>soon</span>
+      </h2>
+      <h3 className="section__subtitle">Sur quoi je travaille actuellement</h3>
+      <div className="container_working">
+        <WorkItems item={project} />
+      </div>
+    </section>
+  );
 };
 
 export default Working;
