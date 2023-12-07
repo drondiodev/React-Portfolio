4024b82ddcf69e4c8d7275ad3a1de901699c5852
DAVID R <103181452+drondiodev@users.noreply.github.com>
2023-12-07 15:06:40 +0100

    ajout projet GithubScrapper

--- src/components/projects/DataProjects.jsx
+++ src/components/projects/DataProjects.jsx
@@ -1,54 +1,60 @@
-import Image1 from '../../assets/nextjs2.webp'
-import Image2 from '../../assets/squeezeEvents.webp'
-import Image3 from '../../assets/projetASTRO.webp'
-import Image4 from '../../assets/projetCV.webp'
-import Image5 from '../../assets/projetQRCODE.webp'
+import Image1 from "../../assets/nextjs2.webp";
+import Image2 from "../../assets/squeezeEvents.webp";
+import Image3 from "../../assets/projetASTRO.webp";
+import Image4 from "../../assets/projetCV.webp";
+import Image5 from "../../assets/projetQRCODE.webp";
+import Image6 from "../../assets/githubScraper.webp";
 
 export const Data = [
-    {
-      id: 1,
-      image: Image1,
-      title: "Full Stack Next.JS 13 | Projet de plateforme E-commerce",
-      alt: "Image du Projet de plateforme E-commerce",
-      description:
+  {
+    id: 1,
+    image: Image1,
+    title: "Full Stack Next.JS 13 | Projet de plateforme E-commerce",
+    alt: "Image du Projet de plateforme E-commerce",
+    description:
       "Création from scratch, d'un site E-commerce, en utilisant Next.JS 13, TailwindCSS, TypeScript, Supabase, Stripe, Clerck et Shadcn-UI.",
-      repo: "https://github.com/drondiodev/",
-    },
-    {
-      id: 2,
-      image: Image2,
-      title: "Full Stack Next.JS 13 | Squeeze Events",
-      alt: "Image du Projet Squeeze Events",
-      description:
-        "Projet en binôme, de création d'une plateforme de génération d'E-billets pour événements tech - En cours de construction.",
-      repo: "https://github.com/drondiodev/event-app",
-    },
-    {
-      id: 3,
-      image: Image3,
-      title: "Exemple de portfolio fait avec Astro",
-      alt: "Image de mon ancien portfolio fait avec Astro",
-      description:
-        "Première itération de mon portfolio, réalisé avec Astro.",
-      repo: "https://github.com/drondiodev/portfolio",
-    },
-    {
-      id: 4,
-      image: Image4,
-      title: "Mon CV en HTML & SCSS",
-      alt: "Image de précédent CV en HTML & SCSS",
-      description:
-        "Mon premier vrai projet en HTML et SCSS.",
-      repo: "https://github.com/drondiodev/drondiodev.github.io",
-    },
-    {
-      id: 5,
-      image: Image5,
-      title: "Qr-code-component",
-      alt: "Image du projet Qr-code-component",
-      description:
-        "Challenge Frontend Mentor - Projet QR Code component en HTML et CSS.",
-      repo: "https://github.com/drondiodev/QR-code-component",
-    },
-  ];
-  
+    repo: "https://github.com/drondiodev/",
+  },
+  {
+    id: 2,
+    image: Image6,
+    title: "My Github Repo Scraper",
+    alt: "Image du Projet Github Scraper",
+    description: "Projet de création d'un site pour scrapper des repos Github.",
+    repo: "https://github.com/drondiodev/GithubScraper",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Full Stack Next.JS 13 | Squeeze Events",
+    alt: "Image du Projet Squeeze Events",
+    description:
+      "Projet en binôme, de création d'une plateforme de génération d'E-billets pour événements tech - En cours de construction.",
+    repo: "https://github.com/drondiodev/event-app",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Exemple de portfolio fait avec Astro",
+    alt: "Image de mon ancien portfolio fait avec Astro",
+    description: "Première itération de mon portfolio, réalisé avec Astro.",
+    repo: "https://github.com/drondiodev/portfolio",
+  },
+  {
+    id: 4,
+    image: Image4,
+    title: "Mon CV en HTML & SCSS",
+    alt: "Image de précédent CV en HTML & SCSS",
+    description: "Mon premier vrai projet en HTML et SCSS.",
+    repo: "https://github.com/drondiodev/drondiodev.github.io",
+  },
+  {
+    id: 5,
+    image: Image5,
+    title: "Qr-code-component",
+    alt: "Image du projet Qr-code-component",
+    description:
+      "Challenge Frontend Mentor - Projet QR Code component en HTML et CSS.",
+    repo: "https://github.com/drondiodev/QR-code-component",
+  },
+];
--- src/components/workingproject/working.jsx
+++ src/components/workingproject/working.jsx
@@ -3,15 +3,20 @@ import WorkItems from "../work/WorkItems";
 import { Data } from "../projects/DataProjects";
 
 const Working = () => {
-  const project = Data.find((p) => p.id === 1);
+  // const project = Data.find((p) => p.id === 2);
   return (
     <section className="working section">
       <h2 className="section__title">
         Coming <span>soon</span>
       </h2>
       <h3 className="section__subtitle">Sur quoi je travaille actuellement</h3>
-      <div className="container_working">
+      {/* <div className="container_working">
         <WorkItems item={project} />
+      </div> */}
+      <div className="container_working">
+        {Data.slice(0, 2).map((project) => (
+          <WorkItems key={project.id} item={project} />
+        ))}
       </div>
     </section>
   );
