90e213e2eb70927276547745409c84a5cad3307e
DAVID R <103181452+drondiodev@users.noreply.github.com>
2024-01-05 00:12:46 +0100

    add slogan

--- src/components/appointment/Appointment.jsx
+++ src/components/appointment/Appointment.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./appointment.css";
 import pastille from "../../assets/pastille.svg";
+import arrow2 from "../../assets/arrow2.svg";
 
 const Appointment = () => {
   const [message, setMessage] = useState("Une question ?");
@@ -8,7 +9,7 @@ const Appointment = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setMessage((prev) => {
-        return prev === "Une question ?" ? "Un devis ?" : "Une question ?";
+        return prev === "Une question ?" ? "Prendre un RDV" : "Une question ?";
       });
     }, 3000);
 
@@ -16,22 +17,32 @@ const Appointment = () => {
   }, []);
   return (
     <section className="home__appointment">
-      <div className="appointment">
+      {/* <div className="appointment">
         <a href="#contact" title="message" className="appointment-link">
           <span>{message}</span>
         </a>
-      </div>
-      <div className="appointment">
-        <a
-          href="https://calendly.com/drondiodev/"
-          target="_blank"
-          rel="noreferrer"
-          title="Prendre un RDV"
-          className="appointment-link"
-        >
-          <span>Prendre un RDV</span>
-        </a>
-        <img src={pastille} alt="" className="pastille" />
+      </div> */}
+      <div className="home__appointmentarrow">
+        <div className="top">
+          <h2>Passez à l&apos;action </h2>
+          <div>
+            <img src={arrow2} alt="fleche" width={110} />
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="appointment">
+            <a
+              href="https://calendly.com/drondiodev/"
+              target="_blank"
+              rel="noreferrer"
+              title="Prendre un RDV"
+              className="appointment-link"
+            >
+              <span>{message}</span>
+            </a>
+            <img src={pastille} alt="" className="pastille" />
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -2,10 +2,13 @@ const Data = () => {
   return (
     <section aria-label="Information principale">
       <h1 className="home__title">
-        Hello, I&apos;m <span>David</span> Rondio
+        <span>David</span> Rondio
       </h1>
       <p className="home__subtitle">
         <span>Business </span>&amp; Web <span>Developer</span>
+      </p>
+      <p className="home__slogan">
+        De l&apos;idée au succès en ligne : je développe, vous brillez !
       </p>
       <p className="home__description">
         Je <span>conçois</span> des sites web axés <span>business</span>,
